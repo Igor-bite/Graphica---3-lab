59b75379536a0f21b71798f9831c0fd53bfa66eb
Клюжев И. А <klyuzhevigor@gmail.com>
2020-04-05 15:10:44 +0300

    Считывание аргументов

--- Graphica 3 lab/main.cpp
+++ Graphica 3 lab/main.cpp
@@ -7,13 +7,33 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 using namespace std;
 
+typedef unsigned char uchar;
+
 int main(int argc, const char * argv[]) {
-    cout << "Hello, World!\n";
-    cout << "1 dithering method\n";
+    if (argc < 9 || argc > 10) {
+        cerr << "Incorrect count of arguments" << endl;
+        return 0;
+    }
+
+    string inputFileName = argv[1];
+    string outputFileName = argv[2];
+    uchar color = stoi(argv[3]);
+    float thickness = stof(argv[4]);
+    float x0 = stof(argv[5]);
+    float y0 = stof(argv[6]);
+    float x1 = stof(argv[7]);
+    float y1 = stof(argv[8]);
     
-    
-    cout << "image detection" << endl;
+    float gamma = 2.2;
+    bool gammaDefined = false;
+    if (argc == 10) {
+        gamma = stof(argv[9]);
+        gammaDefined = true;
+    }
     return 0;
 }
