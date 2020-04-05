109e88ad6f455db62705a2c8a786478fa20ae19d
Клюжев И. А <klyuzhevigor@gmail.com>
2020-04-05 14:49:12 +0300

    Initial Commit

---
+++ Graphica 3 lab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graphica 3 lab
+//
+//  Created by Игорь Клюжев on 05.04.2020.
+//  Copyright © 2020 Игорь Клюжев. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
